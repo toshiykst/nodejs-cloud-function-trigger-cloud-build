ee12bd24a67e2ae9d48895c8fdfb2e1656cd4abc
toshiykst <yxkxtxsx1075@gmail.com>
2021-02-04 15:01:31 +0900

    Implement trigger cloud build function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
-import { Request, Response } from 'express'
+import { CloudBuildClient } from "@google-cloud/cloudbuild";
+import { Request, Response } from "express";
 
-export const triggerCloudBuild = (_req: Request, res: Response) => {
-  const message = "Hello World!";
-  res.status(200).send(message);
-}
+const PROJECT_ID = "YOUR_PROJECT_ID";
+const TRIGGER_ID = "YOUR_CLOUD_BUILD_TRIGGER_ID";
+const BRANCH_NAME = "BRANCH_NAME"; // e.g. "main"
+
+export const triggerCloudBuild = async (_req: Request, res: Response) => {
+  const cb = new CloudBuildClient();
+
+  try {
+    // Starts a build against the branch provided.
+    await cb.runBuildTrigger({
+      projectId: PROJECT_ID,
+      triggerId: TRIGGER_ID,
+      source: {
+        projectId: PROJECT_ID,
+        dir: "./",
+        branchName: BRANCH_NAME,
+      },
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+    return;
+  }
+
+  res.status(200).send("triggered cloud build!");
+};
